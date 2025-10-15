d1d71ec0ef5483ac3574c8b95a8439aebfadf658
Ali Shahbaz <147451904+academy-redmaster@users.noreply.github.com>
2025-10-15 19:35:08 +0330

    Completed lesson on text-related HTML tags

---
+++ main.js
@@ -0,0 +1,8 @@
+// document.getElementById("myapp").innerHTML="Goodbye"
+// alert("main.js hello")
+console.log("main.js start running file")
+const start = Date.now()
+while(Date.now() - start < 10000){}
+console.log("main.js end running file")
+document.getElementById("myapp").innerHTML="Goodbye"
+
